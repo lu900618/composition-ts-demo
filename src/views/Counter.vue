0f0488990aa867fa496fc4b72bc153fe72958292
lu900618 <32401545@qq.com>
2020-05-10 17:52:15 +0800

    counter

--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -1,13 +1,37 @@
 <template>
-  <div>counter</div>
+  <div>{{ count }}</div>
+  <button @click="addCount(2)">mapMutations add</button>
+  <button @click="asyncAddCount(2)">mapActions add</button>
+  <button @click="asyncAdd">dispatch asyncAddCount</button>
+  <button @click="reduceSync">commit reduce</button>
 </template>
 
-<script lan="ts">
+<script lang="ts">
+// eslint-disable-next-line no-unused-vars
+import { useStore, Store, mapMutations, mapActions } from "vuex";
+// eslint-disable-next-line no-unused-vars
+import { State } from "../store";
+import { computed } from "vue";
 export default {
+  setup() {
+    const store: Store<State> = useStore();
+    const count = computed(() => store.getters.getCount);
 
-}
+    function reduceSync() {
+      store.commit("reduceCount", 4);
+    }
+    function asyncAdd() {
+      store.dispatch("asyncAddCount", 4);
+    }
+    return {
+      count,
+      reduceSync,
+      asyncAdd,
+      ...mapMutations(["addCount"]),
+      ...mapActions(["asyncAddCount"]),
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
